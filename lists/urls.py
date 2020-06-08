1317ea61588fad31a986bee0d6ee8966d325dddb
Silenzen <243232325@qq.com>
2020-06-08 23:54:11 +0800

    重构列表视图以处理新项目发布

--- lists/urls.py
+++ lists/urls.py
@@ -20,6 +20,5 @@ from . import views
 app_name = 'lists'
 urlpatterns = [
     path('<int:list_id>/', views.view_list, name='view_list'),
-    path('<int:list_id>/add_item/', views.add_item, name='add_item'),
     path('new/', views.new_list, name='new_list'),
 ]
