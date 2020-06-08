ed5df036ed0c5f5e2fdd157e5eb8b2edbaea77f2
Silenzen <243232325@qq.com>
2020-06-08 19:30:44 +0800

    在模型层进行校验

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -1,4 +1,4 @@
-
+from django.core.exceptions import ValidationError
 from django.test import TestCase
 from lists.models import Item, List
 import re
@@ -11,7 +11,7 @@ def remove_csrf(response, expected_html):
     expected_html = re.sub(csrf_regex, '', expected_html)
     return observed_html, expected_html
 
-class ListViewTest(TestCase):
+class ListAndViewTest(TestCase):
 
     def test_passes_correct_list_to_template(self):
         other_list = List.objects.create()
@@ -65,35 +65,9 @@ class ListViewTest(TestCase):
         self.assertNotContains(response, 'other list item 1')
         self.assertNotContains(response, 'other list item 2')
 
-
-class NewListTest(TestCase):
-    
-    def test_saving_a_POST_request(self):
-        self.client.post('/lists/new/', data={'item_text': 'A new list item'})
-        self.assertEqual(Item.objects.count(), 1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, 'A new list item')
-
-    def test_redirects_after_POST(self):
-        response = self.client.post('/lists/new/', data={'item_text': 'A new list item'})
-        new_list = List.objects.first()
-        self.assertRedirects(response, '/lists/%d/' % new_list.id)
-
-    def test_can_save_a_POST_request_to_an_existing_list(self):
-        other_list = List.objects.create()
-        correct_list = List.objects.create()
-        
-        self.client.post('/lists/%d/add_item/' % (correct_list.id), data={'item_text': 'A new item for an existing list'})
-
-        self.assertEqual(Item.objects.count(), 1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, 'A new item for an existing list')
-        self.assertEqual(new_item.list, correct_list)
-    
-    def test_redirects_to_list_view(self):
-        other_list = List.objects.create()
-        correct_list = List.objects.create()
-
-        response = self.client.post('/lists/%d/add_item/' % (correct_list.id), data={'item_text': 'A new item for an existing list'})
-
-        self.assertRedirects(response, '/lists/%d/' % correct_list.id)
+    def test_cannot_save_empty_list_items(self):
+            list_ = List.objects.create()
+            item = Item.objects.create(list=list_, text='')
+            with self.assertRaises(ValidationError):
+                item.save()
+                item.full_clean()
