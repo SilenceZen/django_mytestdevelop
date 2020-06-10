a256a4fa0250da95e5f29e730d9a9925185f0c23
Silenzen <243232325@qq.com>
2020-06-10 15:24:38 +0800

    Implement duplicate item validation at model layer

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -11,7 +11,7 @@ def remove_csrf(response, expected_html):
     expected_html = re.sub(csrf_regex, '', expected_html)
     return observed_html, expected_html
 
-class ListAndViewTest(TestCase):
+class ItemModelTest(TestCase):
 
     def test_passes_correct_list_to_template(self):
         other_list = List.objects.create()
@@ -19,31 +19,16 @@ class ListAndViewTest(TestCase):
         response = self.client.get('/lists/%d/' % correct_list.id)
         self.assertEqual(response.context['list'], correct_list)
     
-    def test_saving_and_retrieving_items(self):
-        list_ = List()
-        list_.save()
-        first_item = Item()
-        first_item.text = 'The first (ever) list item'
-        first_item.list = list_
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = 'Item the second'
-        second_item.list = list_
-        second_item.save()
-
-        saved_list = List.objects.first()
-        self.assertEqual(saved_list, list_)
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
-        self.assertEqual(first_saved_item.list, list_)
-        self.assertEqual(second_saved_item.text, 'Item the second')
-        self.assertEqual(second_saved_item.list, list_)
+    def test_default_text(self):
+        item = Item()
+        self.assertEqual(item.text, '')
+    
+    def test_item_is_related_to_list(self):
+        list_ = List.objects.create()
+        item = Item()
+        item.list = list_
+        item.save()
+        self.assertIn(item, list_.item_set.all())
 
     def tests_uses_list_template(self):
         list_ = List.objects.create()
@@ -72,6 +57,26 @@ class ListAndViewTest(TestCase):
                 item.save()
                 item.full_clean()
 
-    def test_page_absolute_url(self):
+    def test_get_absolute_url(self):
+        list_ = List.objects.create()
+        self.assertEqual(list_.get_absolute_url(), '/lists/%d/' % list_.id)
+
+    def test_duplicate_items_are_invalid(self):
+        list_ = List.objects.create()
+        Item.objects.create(list=list_, text='bla')
+        with self.assertRaises(ValueError):
+            item = Item(list=list_, text='bla')
+            item.full_clean()
+
+    def test_CAN_save_item_to_different_lists(self):
+        list1 = List.objects.create()
+        list2 = List.objects.create()
+        Item.objects.create(list=list1, text='bla')
+        item = Item(list=list2, text='bla')
+        item.full_clean()
+
+class ListModelTest(TestCase):
+
+    def test_get_absolute_url(self):
         list_ = List.objects.create()
         self.assertEqual(list_.get_absolute_url(), '/lists/%d/' % list_.id)
